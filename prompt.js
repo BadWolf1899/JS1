b2d160b5f977f060afc2b2d347cd310e3aa38b2b
Valentina <vamlopezeguren@gmail.com>
2023-09-04 20:21:10 -0300

    Avance
    Agregué funciones, array y objetos.

--- prompt.js
+++ prompt.js
@@ -1,33 +1,48 @@
 function calculateMaterial() {
-    // Prompt para solicitar al usuario las medidas de la pared
-    var ancho = parseFloat(prompt("Por favor, ingresá el ancho de la pared en metros:"));
-    var alto = parseFloat(prompt("Ingresá el alto de la pared en metros:"));
-  
-    // Prompt para que el usuario escoja el material
-    var option = prompt("Ingregá 'pintura' o 'empapelado' para calcular el material necesario:");
-  
-    // Validación del input del usuario
-    if (isNaN(ancho) || isNaN(alto) || (option !== 'pintura' && option !== 'empapelado')) {
+  // Prompt para solicitar al usuario las medidas de la pared
+  var ancho = parseFloat(prompt("Por favor, ingresá el ancho de la pared en metros:"));
+  var alto = parseFloat(prompt("Ingresá el alto de la pared en metros:"));
+
+  // Prompt para que el usuario escoja el material
+  var option = prompt("Ingregá 'pintura' o 'empapelado' para calcular el material necesario:");
+
+  // Validación del input del usuario
+  if (isNaN(ancho) || isNaN(alto) || (option !== 'pintura' && option !== 'empapelado')) {
       console.log("Elección inválida. Intenta de nuevo.");
-      calculateMaterial(); // Re-prompt al usuario para una repsuesta válida
+      calculateMaterial(); // Re-prompt al usuario para una respuesta válida
       return;
-    }
-  
-    // Cálculo del área
-    var area = ancho * alto;
-  
-    // Cálculo del material necesario según los datos del usuario
-    var materialNeeded;
-    if (option === 'pintura') {
-      // Si un litro de pinutra cubre 10 metros cuadrados
-      materialNeeded = area / 10;
-    } else if (option === 'empapelado') {
-        // Si el empapelado cubre 5 metros cuadrados por rollo
-        materialNeeded = Math.ceil(area / 5);
+  }
+
+  // Cálculo del área
+  var area = ancho * alto;
+
+  // Creación de un objeto que contiene información sobre los materiales
+  var materials = {
+      pintura: {
+          coverage: 10,
+          unit: "litros"
+      },
+      empapelado: {
+          coverage: 5,
+          unit: "rollos"
       }
-    
-      // Enviamos el resultado
-      prompt("Para tu proyecto vas a necesitar " + materialNeeded + " unidades de " + option + ".");
-    }
-    
-    calculateMaterial();
+  };
+
+  // Función para calcular el material necesario
+  var calculateNeededMaterial = function(area, material) {
+      return Math.ceil(area / material.coverage);
+  };
+
+  // Cálculo del material necesario según los datos del usuario
+  var materialNeeded;
+  if (option === 'pintura') {
+      materialNeeded = calculateNeededMaterial(area, materials.pintura);
+  } else if (option === 'empapelado') {
+      materialNeeded = calculateNeededMaterial(area, materials.empapelado);
+  }
+
+  // Enviamos el resultado
+  console.log("Para tu proyecto vas a necesitar " + materialNeeded + " unidades de " + materials[option].unit + " de " + option + ".");
+}
+
+calculateMaterial();
